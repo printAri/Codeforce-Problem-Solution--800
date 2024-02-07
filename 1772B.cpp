43eacad69a457a59b3a75e1ecd8e208e9e756ebd
Aritra dev <147065581+printAri@users.noreply.github.com>
2024-02-07 19:25:22 +0600

    Create 1772B.cpp

---
+++ 1772B.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if((a<b) && (c<d) && (a<c) && (b<d)){
+            cout<<"YES\n";
+        }
+        else if((a<b)&& (c<d) && (a>c) && (b>d)){
+            cout<<"YES\n";
+        }
+        else if((a>b) && (c>d) && (a<c) && (b<d))
+        cout<<"YES\n";
+        else if((a>b) && (c>d) && (a>c) && (b>d)){
+            cout<<"YES\n";
+        }
+        else
+        cout<<"NO\n";
+    }
+}
